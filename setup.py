f132652c4a756d33dd6585b875b079d8e936253e
bquangDinh <buiquangdinh1751@gmail.com>
2019-01-10 14:53:13 +0700

    Install some needing packages

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+def install_dlib():
+	os.system("sudo pip install scipy")
+
+	os.system("sudo pip install scikit-image")
+
+	os.system("sudo pip install dlib")
+
+def install_cv2():
+	os.system("sudo pip install opencv-python")
+
+def install_imutils():
+
+	os.system("sudo pip install imutils")
+
+def install_packages():
+	install_dlib()
+	install_cv2()
+	install_imutils()
+
+	print "Ready to use face_recognition_testing.py. Use the command 'python face_recognition_testing.py --help' to see the parameters"
+
+	
+
+
+install_packages()
