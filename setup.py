051e7a982a7591f38db9cd9aa49813a37e2d151a
bquangDinh <buiquangdinh1751@gmail.com>
2019-01-11 12:35:57 +0700

    add face_recognition package

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ def install_imutils():
 
 	os.system("sudo pip install imutils")
 
+def install_face_recognition():
+
+	os.system("sudo pip install face_recognition")
+
 def install_packages():
 	install_dlib()
 	install_cv2()
 	install_imutils()
+	install_face_recognition()
 
 	print "Ready to use face_recognition_testing.py. Use the command 'python face_recognition_testing.py --help' to see the parameters"
 
